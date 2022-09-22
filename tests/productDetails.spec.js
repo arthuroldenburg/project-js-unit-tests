3e9625fa4a99b7569f8926262b396e2028639cb7
Arthur Murilo Oldenburg <oldenburgarthurmurilo@gmail.com>
2022-09-22 09:42:42 -0300

    requisito 6 feito

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -38,9 +38,14 @@ describe('6 - Implemente os casos de teste para a função `productDetails`', ()
     // Teste se o retorno da função é um array.
     expect(productDetails).toHaveProperty('name')
     // Teste se o array retornado pela função contém dois itens dentro.
+    expect(productDetails('first', 'second').length).toBe(2)
     // Teste se os dois itens dentro do array retornado pela função são objetos.
+    expect(typeof productDetails('first', 'second')[0]).toBe('object')
+    expect(typeof productDetails('first', 'second')[1]).toBe('object')
     // Teste se quando passado parâmetros diferentes entre si, os dois objetos também são diferentes entre si.
+    expect(productDetails('first', 'second')[0]).not.toBe(productDetails('first', 'second')[1])
     // Teste se os dois productIds terminam com 123.
-    expect(productDetails()).toContain('123')
+    expect(productDetails('first', 'second')[0].details.productId).toContain(123)
+    expect(productDetails('first', 'second')[1].details.productId).toContain(123)
   });
 });
